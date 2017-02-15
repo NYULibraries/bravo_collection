57d51dc4e63628ca1557748863c455122a1773ef
hab278 <hab278@nyu.edu>
2017-02-15 13:16:13 -0500

    Change ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 require 'formaggio/capistrano'
 set :recipient, "web.services@library.nyu.edu"
 set :app_title, "bravo_collection"
-set :rvm_ruby_string, "2.3.1"
+set :rvm_ruby_string, "2.2.6"
 
 set :new_relic_environments, []
 
